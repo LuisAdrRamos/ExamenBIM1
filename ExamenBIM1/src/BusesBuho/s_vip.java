324df072e8c9d5738414a917848420004b98a1f5
Luis Adrian Ramos <150483323+keepchup@users.noreply.github.com>
2024-06-14 13:02:23 -0500

    Add files via upload

--- ExamenBIM1/src/BusesBuho/s_vip.java
+++ ExamenBIM1/src/BusesBuho/s_vip.java
@@ -1,5 +1,7 @@
 package BusesBuho;
 
+import java.util.Scanner;
+
 public class s_vip extends t_vip{
     cliente pasajero1;
 
@@ -11,10 +13,18 @@ public class s_vip extends t_vip{
     }
 
     public void serviciosVIP() {
+        Scanner sc = new Scanner(System.in);
         System.out.println("Servicios VIP");
         System.out.println("Usted dispone de Television e internet incluido con su ticket");
-        System.out.println("Usted dispone de 2 espacios para 2 maletas");
+        System.out.println("Usted dispone de espacio para 2 maletas");
         System.out.println("Puede añadir un espacio extra para otra maleta por un cargo adicional");
-        
+
+        System.out.println("¿Desea añadir un espacio adicional por $5?\n\t1. Si\n\t2. No");
+        int op = sc.nextInt();
+        if (op==1){
+            pasajero1.precio_Ticket= precio_Ticket+5;
+        } else {
+            System.out.println("Entendible, muchas gracias");
+        }
     }
 }
